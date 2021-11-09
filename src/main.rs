f502cd53a26d7e98028d19fd16a7b4d5f4a96fcc
Antonino Bertulla <abertulla@gmail.com>
2021-11-09 23:25:27 +0100

    feat: remove dependencies

--- src/main.rs
+++ src/main.rs
@@ -17,33 +17,50 @@ struct Opt {
     #[structopt(default_value = "", long)]
     suffix: String,
 
-    /// New value for package.json's field `license`
+    /// Update value for package.json's field `license`
     #[structopt(long)]
-    field_license: Option<String>,
+    update_license: Option<String>,
 
-    /// New value for package.json's field `private`
+    /// Update value for package.json's field `private`
     #[structopt(long)]
-    field_private: Option<bool>,
+    update_private: Option<bool>,
 
-    /// New value for package.json's field `peerDependencies`
+    /// Update value for package.json's field `peerDependencies`
     #[structopt(long, parse(try_from_str = parse_key_val))]
-    field_peer_dependencies: Option<Vec<(String, String)>>,
+    update_peer_dependencies: Option<Vec<(String, String)>>,
 
-    /// New value for package.json's field `devDependencies`
+    /// Update value for package.json's field `devDependencies`
     #[structopt(long, parse(try_from_str = parse_key_val))]
-    field_dev_dependencies: Option<Vec<(String, String)>>,
+    update_dev_dependencies: Option<Vec<(String, String)>>,
 
-    /// New value for package.json's field `dependencies`
+    /// Update value for package.json's field `dependencies`
     #[structopt(long, parse(try_from_str = parse_key_val))]
-    field_dependencies: Option<Vec<(String, String)>>,
+    update_dependencies: Option<Vec<(String, String)>>,
+
+    /// Remove value from package.json's field `peerDependencies`
+    #[structopt(long)]
+    remove_peer_dependencies: Option<Vec<String>>,
+
+    /// Remove value from package.json's field `dependencies`
+    #[structopt(long)]
+    remove_dev_dependencies: Option<Vec<String>>,
+
+    /// Remove value from package.json's field `dependencies`
+    #[structopt(long)]
+    remove_dependencies: Option<Vec<String>>,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let opt: Opt = Opt::from_args();
-    let license = opt.field_license.as_deref();
-    let new_peer_dependencies = opt.field_peer_dependencies.as_deref();
-    let new_dev_dependencies = opt.field_dev_dependencies.as_deref();
-    let new_dependencies = opt.field_dependencies.as_deref();
+    let new_license = opt.update_license.as_deref();
+    let new_peer_dependencies = opt.update_peer_dependencies.as_deref();
+    let new_dev_dependencies = opt.update_dev_dependencies.as_deref();
+    let new_dependencies = opt.update_dependencies.as_deref();
+
+    let remove_peer_dependencies = opt.remove_peer_dependencies.as_deref();
+    let remove_dev_dependencies = opt.remove_dev_dependencies.as_deref();
+    let remove_dependencies = opt.remove_dependencies.as_deref();
+
     println!("Arguments: {:?}", opt);
 
     for entry in fs_walker::walk(opt.input_dir) {
@@ -56,17 +73,21 @@ fn main() -> Result<(), Box<dyn Error>> {
                 let data = std::fs::read_to_string(path).expect("Unable to read file");
                 let mut package_json: PackageJson = PackageJson::load(&data).expect(&format!("Unable to parse {}", path.to_str().unwrap()));
 
-                update_single_field(&mut package_json.license, license);
-                update_single_field(&mut package_json.private, opt.field_private);
+                update_single_field(&mut package_json.license, new_license);
+                update_single_field(&mut package_json.private, opt.update_private);
 
                 update_map_field(&mut package_json.peer_dependencies, new_peer_dependencies);
                 update_map_field(&mut package_json.dev_dependencies, new_dev_dependencies);
                 update_map_field(&mut package_json.dependencies, new_dependencies);
+
+                remove_map_field(&mut package_json.peer_dependencies, remove_peer_dependencies);
+                remove_map_field(&mut package_json.dev_dependencies, remove_dev_dependencies);
+                remove_map_field(&mut package_json.dependencies, remove_dependencies);
                 
                 let destination = format!("{}{}", path.to_str().unwrap(), opt.suffix);
                 std::fs::write(destination, PackageJson::save(&package_json).expect("Error during writing file to disk"))?;
 
-                println!("{:?}", "DONE\n");
+                println!("{:?}", "DONE");
             }
         }
     }
@@ -94,6 +115,19 @@ fn update_map_field<'a>(maybe_field: &mut Option<HashMap<&'a str, &'a str>>, may
     }
 }
 
+fn remove_map_field<'a>(maybe_field: &mut Option<HashMap<&'a str, &'a str>>, maybe_new_value: Option<&'a [String]>) {
+    if let Some(new_value) = maybe_new_value {
+        maybe_field.as_mut().and_then(|field| {
+            for k in new_value {
+                let key: &str = k.as_ref();
+                if field.contains_key(key) {
+                    field.remove(key);
+                }
+            }
+            Some(field)
+        });
+    }
+}
 
 fn parse_key_val<T, U>(s: &str) -> Result<(T, U), Box<dyn Error>>
 where
